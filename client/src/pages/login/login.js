909098322aa29acf4410cbdc2db09fb7bcb6563f
nbargo <nsbargo@gmail.com>
2017-10-01 18:47:43 -0700

    built forms and nav components; modified app.js to import nav

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class LoginPage extends Component {
   render() {
-    return (
+    return (   
        <div className="container">
          <div className="row">
            <div className="col-md-6">
