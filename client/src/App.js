1610de4c255445e1abcc6ff7944453649ecb2129
Alexis Frank <time2emit@gmail.com>
2017-10-04 16:30:19 -0700

    stashing for later

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import LoginPage from './pages/login';
+import HomePage from './pages/home';
+import PlayMateDescriptionPage from './pages/detailedplaymatedescription';
+import CreatedDogProfilePage from './pages/createdogprofile';
+import SearchResultsPage from './pages/searchresultspage';
+import Nav from './components/Navbar';
+import Footer from './components/Footer'
+
+const App = () =>
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/playmate" component={PlayMateDescriptionPage} />
+        <Route exact path="/createdogprofile" component={CreatedDogProfilePage} />
+        <Route exact path="/search" component={SearchResultsPage} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>;
+
+export default App;
+
+
+
 // import React, { Component } from 'react';
 // import './App.css';
 // import LoginPage from './pages/login';
@@ -28,33 +58,5 @@
 // export default App;
 
 
-
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import LoginPage from './pages/login';
-import HomePage from './pages/home';
-import PlayMateDescriptionPage from './pages/detailedplaymatedescription';
-import CreatedDogProfilePage from './pages/createdogprofile';
-import SearchResultsPage from './pages/searchresultspage';
-import Nav from './components/Navbar';
-import Footer from './components/Footer'
-
-const App = () =>
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/playmate" component={PlayMateDescriptionPage} />
-        <Route exact path="/createdogprofile" component={CreatedDogProfilePage} />
-        <Route exact path="/search" component={SearchResultsPage} />
-      </Switch>
-      <Footer />
-    </div>
-  </Router>;
-
-export default App;
 
 
