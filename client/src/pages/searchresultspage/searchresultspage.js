d3495beebdf1f4fa3c2080ef35bbeeb6ff6a0e53
Alexis Frank <time2emit@gmail.com>
2017-10-05 18:44:00 -0700

    Help from Jayson and Coop

--- client/src/pages/searchresultspage/searchresultspage.js
+++ client/src/pages/searchresultspage/searchresultspage.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
+//need axios at the level, normally component, where the info is rendered
+//the route has to already be defined in express (separate file)
+//can have another file called APIs which have the call API functions so
+//you only need to call them in this file
+
+import axios from "axios";
 
 class SearchResultsPage extends Component {
+  constructor(props);
+  super(props);
+    this.state = {
+      puppylist: []
+  }
+  componentDidMount = () => {
+    var self = this;
+    API.getpics().then(docs)==>(
+      this.setState({houses: docs.data}));
+  }
+  callAPI = () => {
+    axios.get("/allpuppies").then((response)=>{
+      this.setState({puppylist: response.data})
+    })
+  }
   render() {
     return (
        <div className="container">
@@ -8,6 +29,7 @@ class SearchResultsPage extends Component {
            <div className="col-md-6">
              <div className="Jumbotron">
                <h1>Welcome to SearchResultsPage</h1>
+
              </div>
              <div className="form">
                <div className="formbtn">
